5928766c4183b272814b1b88f9ef9170f7d3c4b5
Maxim Shatalov <shatalov04@gmail.com>
2017-02-24 15:44:57 +0300

    #hw.3 animation parallax problem

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,29 @@
-(function() {
-  'use strict';
+'use strict';
+// Modules
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+var parallax = (function () {
+    var bg = document.querySelector('.l-developer__bg'),
+        user = document.querySelector('.l-developer__container');
+
+    return {
+        move: function (block, windowScroll, strafeCoefficient) {
+            var strafe = -(windowScroll / strafeCoefficient) + '%';
+            var style = block.style;
+            // Var for rendering by video processor (z-axis)
+            var transformString = 'translate3d(0, ' + strafe + ', 0)';
+            style.transform = transformString;
+            style.webkitTransform = transformString;
+        },
+        init: function (winScroll) {
+            this.move(bg, winScroll, 100);
+            this.move(user, winScroll, 7);
+        }
+    }
 })();
+
+// Events
+window.onscroll = function () {
+    var winScroll = window.pageYOffset;
+
+    parallax.init(winScroll);
+}
